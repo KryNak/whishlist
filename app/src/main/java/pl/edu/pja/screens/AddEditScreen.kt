5ef4c7eff46f3ee67cc544bf988682813efcc6a4
krynak <krystian.nakielski200397@gmail.com>
2022-06-07 01:41:00 +0200

    Added google maps

--- app/src/main/java/pl/edu/pja/screens/AddEditScreen.kt
+++ app/src/main/java/pl/edu/pja/screens/AddEditScreen.kt
@@ -1,15 +1,27 @@
+@file:OptIn(ExperimentalMaterialApi::class)
+
 package pl.edu.pja.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import pl.edu.pja.Datasource
 import pl.edu.pja.R
 import pl.edu.pja.models.ItemModel
@@ -35,12 +47,35 @@ fun AddEditScreen(navController: NavController, mode: String, itemId: Int) {
 
 @Composable
 fun AppScaffold(navController: NavController, mode: String, itemId: Int) {
+    val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = BottomSheetState(BottomSheetValue.Collapsed)
+    )
+    val coroutineScope = rememberCoroutineScope()
+
     WhishlistTheme {
         Scaffold(
             topBar = {
                 TopAppBar(
                     title = {
                         Text(text = "Whishlist")
+                    },
+                    actions = {
+                        IconButton(
+                            onClick = {
+                                if (!bottomSheetScaffoldState.bottomSheetState.isCollapsed) {
+                                    coroutineScope.launch {
+                                        bottomSheetScaffoldState.bottomSheetState.collapse()
+                                    }
+                                }
+                            }
+                        ) {
+                            if (!bottomSheetScaffoldState.bottomSheetState.isCollapsed) {
+                                Icon(
+                                    imageVector = Icons.Default.ArrowDropDown,
+                                    contentDescription = ""
+                                )
+                            }
+                        }
                     }
                 )
             }
@@ -48,14 +83,27 @@ fun AppScaffold(navController: NavController, mode: String, itemId: Int) {
             Surface(
                 modifier = Modifier.padding(contentPadding)
             ) {
-                Body(navController, mode, itemId)
+                Body(
+                    navController = navController,
+                    mode = mode,
+                    itemId = itemId,
+                    bottomSheetScaffoldState = bottomSheetScaffoldState,
+                    coroutineScope = coroutineScope
+                )
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun Body(navController: NavController, mode: String, itemId: Int) {
+fun Body(
+    navController: NavController,
+    mode: String,
+    itemId: Int,
+    bottomSheetScaffoldState: BottomSheetScaffoldState,
+    coroutineScope: CoroutineScope
+) {
 
     var description by remember {
         mutableStateOf(
@@ -68,32 +116,82 @@ fun Body(navController: NavController, mode: String, itemId: Int) {
         )
     }
 
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 10.dp)
+    val markerPosition = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(markerPosition, 15f)
+    }
+
+    BottomSheetScaffold(
+        scaffoldState = bottomSheetScaffoldState,
+        sheetContent = {
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+            ) {
+                GoogleMap(
+                    modifier = Modifier.fillMaxSize(),
+                    cameraPositionState = cameraPositionState
+                ) {
+                    Marker(
+                        position = markerPosition,
+                        title = "Your Title",
+                        snippet = "Place Name"
+                    )
+                }
+            }
+
+        },
+        sheetPeekHeight = 0.dp,
+        sheetGesturesEnabled = false
     ) {
-        OutlinedTextField(
-            value = description,
-            onValueChange = {
-                description = it
-            },
-            label = { Text(text = "Description") },
-            modifier = Modifier.fillMaxWidth()
-        )
-        OutlinedTextField(
-            value = address,
-            onValueChange = {
-                address = it
-            },
-            label = { Text(text = "Location") },
-            modifier = Modifier.fillMaxWidth()
-        )
-        when (mode) {
-            "ADD" -> AddRow(navController = navController, address, description)
-            "EDIT" -> EditRow(navController = navController, address, description, itemId)
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 10.dp)
+        ) {
+            OutlinedTextField(
+                value = description,
+                onValueChange = {
+                    description = it
+                },
+                label = { Text(text = "Description") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = address,
+                onValueChange = {
+                    address = ""
+                },
+                label = { Text(text = "Location") },
+                modifier = Modifier.fillMaxWidth(),
+                trailingIcon = {
+                    Icon(imageVector = Icons.Default.LocationOn,
+                        contentDescription = "",
+                        tint = Color.Black,
+                        modifier = Modifier.clickable {
+                            coroutineScope.launch {
+
+                                if (bottomSheetScaffoldState.bottomSheetState.isCollapsed) {
+                                    bottomSheetScaffoldState.bottomSheetState.expand()
+                                } else {
+                                    bottomSheetScaffoldState.bottomSheetState.collapse()
+                                }
+
+                            }
+                        }
+                    )
+                }
+            )
+            when (mode) {
+                "ADD" -> AddRow(navController = navController, address, description)
+                "EDIT" -> EditRow(navController = navController, address, description, itemId)
+            }
         }
+
     }
 
 }
@@ -118,7 +216,14 @@ fun EditRow(navController: NavController, address: String, description: String, 
             onClick = {
                 val foundedProduct = Datasource.items.find { it.id == itemId }
                 Datasource.items.removeIf { it.id == itemId }
-                Datasource.items.add(ItemModel(itemId, description, address, foundedProduct?.image ?: R.drawable.ic_add))
+                Datasource.items.add(
+                    ItemModel(
+                        itemId,
+                        description,
+                        address,
+                        foundedProduct?.image ?: R.drawable.ic_add
+                    )
+                )
                 navController.navigate(Screen.MainScreen.route) { popUpTo(0) }
             },
         ) {
